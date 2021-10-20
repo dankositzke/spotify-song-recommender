03b3a7b1f92a6c638e2d2d1cf1941caad8ab8617
JosiahLC <86324031+JosiahLC@users.noreply.github.com>
2021-10-20 12:07:17 -0600

    Update model.py

--- app_folder/model.py
+++ app_folder/model.py
@@ -21,7 +21,7 @@ class Recommendations(DB.Model):
 def find_recommendations(input_feature_vector):
 
     # Read in spotify data from csv
-    df_with_song_names = pd.read_csv("assets/smaller_tracks_features.csv")
+    df_with_song_names = pd.read_csv("smaller_tracks_features.csv")
 
     # Reduce columns to 13 to match the song features that Spotify API will return to us
     cols_to_drop = [
